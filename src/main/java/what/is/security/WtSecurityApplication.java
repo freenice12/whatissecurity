ff191778ac95211aae056afa561e96f494c563aa
freenice12 <freenice12@gmail.com>
2016-10-11 21:40:35 +0900

    first commit

---
+++ src/main/java/what/is/security/WtSecurityApplication.java
@@ -0,0 +1,12 @@
+package what.is.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtSecurityApplication.class, args);
+	}
+}
---
+++ src/test/java/what/is/security/WtSecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package what.is.security;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WtSecurityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
