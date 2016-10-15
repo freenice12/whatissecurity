b7592bd8dc3b3c15b2b4b8a42ecf9d48a2025f5c
freenice12 <freenice12@gmail.com>
2016-10-15 21:13:21 +0900

    create front page Controller

---
+++ src/main/java/what/is/security/controller/MainController.java
@@ -0,0 +1,14 @@
+package what.is.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@GetMapping("/")
+	public String front() {
+		return "MAIN PAGE!";
+	}
+
+}
